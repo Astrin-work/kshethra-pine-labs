c58edf5aec79ce3ff72fbdc3d3ea1333564fff16
user <anuragvenugopalv@gmail.com>
2025-05-19 17:51:36 +0530

    updated date selection in advance booking page

--- lib/view/widgets/booking_page_widget/star_dialodbox_widget.dart
+++ lib/view/widgets/booking_page_widget/star_dialodbox_widget.dart
@@ -25,7 +25,7 @@ class StarDialogBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final currentLang = Provider.of<HomePageViewmodel>(context).currentLanguage;
+
     AppStyles styles = AppStyles();
     SizeConfig().init(context);
     return AlertDialog(
